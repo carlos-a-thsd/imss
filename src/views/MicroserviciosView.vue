ee7ea413ece7b70d3c0d8a7fe0f67188b410284d
carlos-a-thsd <carlos.aldaba@abaton.com.mx>
2023-10-10 14:32:08 -0600

    microservicios view

---
+++ src/views/MicroserviciosView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="m-0 p-0">
+        <h1>Microservicios</h1>
+        <div class="row">
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    h1{
+        
+    }
+</style>
